355ffa90f61017d97f1d0dc8e763e2e0910ad745
Lovesomemadara <borovkun@gmail.com>
2023-12-22 14:08:48 +0500

    Задания выполнил

--- tasks/task_6.py
+++ tasks/task_6.py
@@ -10,7 +10,7 @@ else:
         start, end = end, start
     for i in range(start, end + 1):
         if i > 1:
-            for j in range(2, int(i / 2) + 1):
+            for j in range(2, int(i ** 0.5) + 1):
                 if i % j == 0:
                     break
             else:
--- tasks/task_7.py
+++ tasks/task_7.py
@@ -4,33 +4,50 @@ player1_name: str = input("Игрок 1, введите свое имя: ")
 player2_name: str = input("Игрок 2, введите свое имя: ")
 repeat: str = "да"
 
+winner: str = "Поздравляем! Победитель - "
+
+att_player1: str = (f"ПРЕДУПРЕЖДЕНИЕ: {player1_name} соблюдайте "
+                    f"правила игры. Начнем игру заново!")
+
+att_player2: str = (f"ПРЕДУПРЕЖДЕНИЕ: {player2_name} соблюдайте "
+                    f"правила игры. Начнем игру заново!")
+
+att_all: str = ("ПРЕДУПРЕЖДЕНИЕ: Пожалуйста соблюдайте правила игры. "
+                "Начнем игру заново!")
+
+game: list = ["бумага", "камень", "ножницы"]
+
 while repeat == "да":
 
     player1_choice: str = input(f"{player1_name}: ")
     player2_choice: str = input(f"{player2_name}: ")
 
-    if player1_choice == player2_choice:
-        print("Ничья!")
-    elif player1_choice == "камень":
-        if player2_choice == "ножницы":
-            print(f"Поздравляем! Победитель - {player1_name}!")
-        elif player2_choice == "бумага":
-            print(f"Поздравляем! Победитель - {player2_name}!")
-    elif player1_choice == "ножницы":
-        if player2_choice == "бумага":
-            print(f"Поздравляем! Победитель - {player1_name}!")
-        elif player2_choice == "камень":
-            print(f"Поздравляем! Победитель - {player2_name}!")
-    elif player1_choice == "бумага":
-        if player2_choice == "камень":
-            print(f"Поздравляем! Победитель - {player1_name}!")
-        elif player2_choice == "ножницы":
-            print(f"Поздравляем! Победитель - {player2_name}!")
+    if player1_choice not in game and player2_choice not in game:
+        print(att_all)
+    elif player1_choice not in game:
+        print(att_player1)
+    elif player2_choice not in game:
+        print(att_player2)
     else:
-        print("ПРЕДУПРЕЖДЕНИЕ: Макс соблюдайте правила игры. Начнем игру заново!")
-        continue
+        if ((player1_choice and player2_choice) in game
+                and player1_choice == player2_choice):
+            print("Ничья!")
+        elif player1_choice == game[1]:
+            if player2_choice == game[2]:
+                print(winner, player1_name)
+            elif player2_choice == game[0]:
+                print(winner, player2_name)
+        elif player1_choice == game[2]:
+            if player2_choice == game[0]:
+                print(winner, player1_name)
+            elif player2_choice == game[1]:
+                print(winner, player2_name)
+        elif player1_choice == game[0]:
+            if player2_choice == game[1]:
+                print(winner, player1_name)
+            elif player2_choice == game[2]:
+                print(winner, player2_name)
 
     repeat = input("Хотите продолжить игру? (да/нет): ")
 
-else:
-    print("До встречи!")
+print("До встречи!")
